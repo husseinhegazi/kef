836f1433e979a211d431ca1081179e9be6ec5daf
mohamedElnagar97 <mohamedelnaggar002@gmail.com>
2022-09-09 23:09:11 +0200

    seconde update admin schema

--- models/adminSchema.js
+++ models/adminSchema.js
@@ -1,30 +1,34 @@
 const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
-  FirstName: {
+  firstName: {
     type: String,
     required: true,
     trim: true,
+    minlength: 2,
+    maxlength: 30,
   },
   lastName: {
     type: String,
     required: true,
-    trim: true,
+    minlength: 2,
+    maxlength: 30,
   },
   email: {
     type: string,
     match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-    default: "adminKeef@keef.com",
+    // default: "adminKeef@keef.com",
+    set: "adminKeef@keef.com",
     unique: true,
     required: true,
-    minlength: 10,
     maxlength: 40,
     lowercase: true,
     trim: true,
   },
   password: {
     type: string,
-    default: "Admin@123",
+    // default: "Admin@123",
+    set: "Admin@123",
     required: true,
   },
   phoneNumber: {
