f49f767cc03027e06ee5cc180e8e71ae0c4eaad3
mohamedElnagar97 <mohamedelnaggar002@gmail.com>
2022-10-04 17:25:45 +0200

    products update  validation

--- validations/productsValidation.js
+++ validations/productsValidation.js
@@ -61,3 +61,50 @@ exports.addProductsValidation = [
     .isLength({ max: 5, min: 2 })
     .withMessage("price must be less than 5 numbers long and min 2"),
 ];
+
+exports.updateProductsValidation = [
+  body("name")
+    .isString()
+    .withMessage(" product name must be string")
+    .isLength({ max: 30, min: 2 })
+    .withMessage("product name must be less than 30 characters long and min 2"),
+  body("productInfoId")
+    .optional()
+    .isArray(Number)
+    .withMessage("product info id must be array of numbers"),
+  body("category")
+    .optional()
+    .isIn([
+      "Hoodies",
+      "T-shirts",
+      "Pants",
+      "Chemise",
+      "Tote bags",
+      "Jackets",
+      "Other",
+    ])
+    .withMessage("you can only choose known category")
+    .isString()
+    .withMessage("category must be string"),
+  body("gender")
+    .optional()
+    .isIn(["women", "unisex"])
+    .withMessage("gender can only be wommen or unisex")
+    .isString()
+    .withMessage("gender must be string"),
+  body("sale")
+    .optional()
+    .isBoolean()
+    .withMessage("sale must be true or false only"),
+  body("salePrice")
+    .optional()
+    .isNumeric()
+    .withMessage("sale price must be number")
+    .isLength({ max: 5, min: 2 })
+    .withMessage("sale price must be less than 5 numbers long and min 2"),
+  body("price")
+    .isNumeric()
+    .withMessage("price must be number")
+    .isLength({ max: 5, min: 2 })
+    .withMessage("price must be less than 5 numbers long and min 2"),
+];
