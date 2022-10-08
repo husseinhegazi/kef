07ec7387215c596025057d5e4453d6e5d401a73d
mohamedElnagar97 <mohamedelnaggar002@gmail.com>
2022-10-08 18:47:35 +0200

    handel populate

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 const mwError = require("../MW/validationMW");
 const authMW = require("../MW/authMW");
-const productController=require("../controllers/productController")
-const { updateProductsValidation,addProductsValidation } = require("../validations/productsValidation");
+const productController = require("../controllers/productController");
+const {
+  updateProductsValidation,
+  addProductsValidation,
+} = require("../validations/productsValidation");
 
-
-router.route("/product")
-.get(productController.getAllProducts)
-.post(addProductsValidation,mwError,productController.addNewProduct)
-.delete()
-router.route("/product/:id")
-.get(productController.getProductById)
-.put(updateProductsValidation,mwError,productController.updateProductById)
-.delete()
+router
+  .route("/product")
+  .get(productController.getAllProducts)
+  .post(addProductsValidation, mwError, productController.addNewProduct)
+  .delete();
+router
+  .route("/product/:id")
+  .get(productController.getProductById)
+  .put(updateProductsValidation, mwError, productController.updateProductById)
+  .delete();
 
 module.exports = router;
